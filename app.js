fbf799936ef51783ec7f3e023035dda7752f6c81
Mila Endo <milal.alim@gmail.com>
2017-07-27 09:04:59 -0700

    console log is working

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser')
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.use(express.static(path.join(__dirname, 'static')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.get("/", function(req, res, next){
   res.render("index", {appType:"Express"})
+})
+
+app.post("/", function(req,res,next){
+	let items = []
+	items.push(req.body.todo)
+	console.log(items)
+	res.render("index",items)
 })
 
 app.listen(3000, function(){
